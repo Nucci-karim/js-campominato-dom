796a324185d083add84ea68afbdad31047c7156a
Nucci-karim <nuccikarim@gmail.com>
2023-04-03 15:55:11 +0200

    esercizio finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,13 @@ btnGioca.addEventListener('click', function(){
 function campoMinato(){
     // richiamo valore del select 
     let valoreDifficolta = parseInt(selectDifficolta.value)
+
+    let bombe = generazionebombe( valoreDifficolta)
+
+    let gameOver = false
+    let score = 0
+
+    // reset variabile css 
     variabileDifficolta(valoreDifficolta)
 
     // refresh del main 
@@ -41,10 +48,31 @@ function campoMinato(){
         document.querySelector('.griglia').append(divCella)
 
         divCella.addEventListener('click', function(){
-            console.log(this)
-            this.classList.add('clicked')
+            console.log(i)
+
+            if(!gameOver){
+                // controllo presenza della bomba 
+                if( !bombe.includes(i)){
+                    this.classList.add('clicked')
+                    score++
+                } else{
+                    this.classList.add('clicked-bomb')
+                    gameOver = true
+                    document.querySelector('#game-over').append(document.innerHTML = `Hai Perso: con ${score} punti`)
+                    this.innerHTML = `<i class="fa-solid fa-bomb fa-beat" style="color: #b30000;"></i>`
+                    // alert( 'Hai perso, Riprova!' )
+                    
+                }
+            } else{
+                htmlMain.innerHTML = ''
+                document.querySelector('#game-over').innerHTML = ``
+            }
+            
+            
         })
     }
+
+
 }
 
 function variabileDifficolta(x) {
@@ -52,4 +80,27 @@ function variabileDifficolta(x) {
 
     // aggiunta della variabile del --numeocelle per selezionare la difficolta con il select 
     r.style.setProperty('--numCelle', x);
+}
+
+function generazionebombe(difficoltaParam){
+    const arrayBombe = []
+    
+
+    // creazione 16 bombe 
+    for( let k = 0; arrayBombe.length < 16; k++){
+
+        let bomb = numRandom(1, difficoltaParam)
+         if(!arrayBombe.includes( bomb )){
+            arrayBombe.push (bomb)
+         }
+    }
+
+   
+
+    console.log( arrayBombe )
+    return arrayBombe
+}
+
+function numRandom(min, max){
+    return Math.floor(Math.random() * max) + min
 }
